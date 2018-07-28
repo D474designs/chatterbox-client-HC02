61400927e4fcd001781bd0255f7d68aec60112de
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-07-27 22:22:00 -0500

    add clearMessages functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,10 @@
-var message = {
-  username: 'shawndrost',
-  text: 'trololo',
-  roomname: '4chan'
-};
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
 
-let app = {
+var app = {
   init: function() {
     return null;
   },
@@ -27,7 +27,31 @@ let app = {
 },
 
 fetch: function() {
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: undefined,
+  type: 'GET',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+});
+},
 
+clearMessages: function() {
+  	var node = document.getElementById('chats');
+  	  while (node.firstChild){
+  	  	node.removeChild(node.firstChild);
+  	  }
+},
+
+renderMessage: function(message) {
+    $('#chats').append('<li>' + message.username + ': ' + message.text + '</li>');
 },
 
   // $.post("demo_test_post.asp",
